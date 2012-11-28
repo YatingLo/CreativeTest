96f2e125d730468fe5e8bf64da591a740dca64cf
ChristineLo <yatinglo@yahoo.com.tw>
2012-11-28 19:11:32 +0800

    加入範例引擎

---
+++ ActiveThree/Active3/VCActive3.h
@@ -0,0 +1,13 @@
+//
+//  VCActive3.h
+//  ActiveThree
+//
+//  Created by Mac06 on 12/11/28.
+//  Copyright (c) 2012年 Mac06. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCActive3 : UIViewController
+
+@end
---
+++ ActiveThree/Active3/VCActive3.m
@@ -0,0 +1,38 @@
+//
+//  VCActive3.m
+//  ActiveThree
+//
+//  Created by Mac06 on 12/11/28.
+//  Copyright (c) 2012年 Mac06. All rights reserved.
+//
+
+#import "VCActive3.h"
+
+@interface VCActive3 ()
+
+@end
+
+@implementation VCActive3
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
